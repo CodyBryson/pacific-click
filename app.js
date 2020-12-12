a0b42cd3a3854d38e58c90ada147fdf181f72e9b
CodyBryson <codybryson@hotmail.com>
2020-12-12 15:11:54 -0700

    UI finished

---
+++ app.js
@@ -0,0 +1,15 @@
+//global variables
+let level = 1
+let jaegerHealth = 100
+let cores = 0
+let kaijuHealth = 100
+let missileTracker = 0
+let computerTracker = 0
+let reactorTracker = 0
+let maintTracker = 0
+let missileUpgrade = 1
+let computerUpgrade = 5
+let reactorUpgrade = 10
+let maintUpgrade = 10
+
+function click()
