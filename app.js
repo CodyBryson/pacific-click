e3a41eb278bf241e716487ad8fea0a8fed098e56
CodyBryson <codybryson@hotmail.com>
2020-12-13 14:40:13 -0700

    added draw methods and functions to randomize

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //global variables
 let level = 1;
-let jaegerHealth = 100;
+let jaeger_health = 100;
 let kaijuCores = 0;
 let kaijuHealth = 100;
 
@@ -8,7 +8,8 @@ let clickCount = 0;
 
 let defaultDamage = 10;
 let upgradeMultiplier = 0;
-let automissileTotal = 0;
+let autoMissileTotal = 0;
+let autoDroneTotal = 0;
 let regenTotal = 0;
 
 //Upgrade Dictionary
@@ -17,6 +18,11 @@ let autoUpgrades = {
     price: 5,
     quantity: 0,
     multiplier: 20
+  },
+  drones: {
+    price: 10,
+    quantity: 0,
+    multiplier: 40
   }
 };
 let shootUpgrades = {
@@ -42,11 +48,22 @@ let healthUpgrades = {
 };
 
 
+
+
 function buyMissile() {
   if (kaijuCores >= autoUpgrades.missiles.price) {
     kaijuCores -= autoUpgrades.missiles.price;
     autoUpgrades.missiles.price += 10;
     autoUpgrades.missiles.quantity++;
+    statUpdate()
+  }
+}
+function buyDrone() {
+  if (kaijuCores >= autoUpgrades.drones.price) {
+    kaijuCores -= autoUpgrades.drones.price;
+    autoUpgrades.drones.price += 15;
+    autoUpgrades.drones.quantity++;
+    statUpdate()
   }
 }
 function buyComputer() {
@@ -55,6 +72,7 @@ function buyComputer() {
     upgradeMultiplier += shootUpgrades.computer.multiplier;
     shootUpgrades.computer.price += 5;
     shootUpgrades.computer.quantity++;
+    statUpdate()
   }
 }
 function buyReactor() {
@@ -62,6 +80,7 @@ function buyReactor() {
     kaijuCores -= reactorUpgrades.reactor.price;
     reactorUpgrades.reactor.price += 10;
     reactorUpgrades.reactor.quantity++;
+    statUpdate()
   }
 }
 function buyHealth() {
@@ -71,21 +90,42 @@ function buyHealth() {
     healthUpgrades.health.price *= 2;
     healthUpgrades.health.multiplier *= 2;
     healthUpgrades.health.quantity++;
+    statUpdate()
   }
 }
 function shoot() {
-  kaijuHealth -= (defaultDamage + upgradeMultiplier)
-  console.log(kaijuHealth)
+  kaijuHealth -= (defaultDamage + upgradeMultiplier);
+  clickCount++;
+  statUpdate()
 }
 function collectMissile() {
   let autoTotal = 0;
   autoTotal += (autoUpgrades.missiles.multiplier * autoUpgrades.missiles.quantity);
   autoMissileTotal = autoTotal;
   kaijuHealth -= autoTotal;
+  if (autoTotal > 0) {
+    clickCount++;
+  }
+  statUpdate();
   return autoTotal;
 }
 function startmissileInterval() {
-  var autoInterval = setInterval(collectMissile, 3000);
+  var autoInterval = setInterval(collectMissile, 2500);
+
+}
+function collectDrone() {
+  let droneTotal = 0;
+  droneTotal += (autoUpgrades.drones.multiplier * autoUpgrades.drones.quantity);
+  autoDroneTotal = droneTotal;
+  kaijuHealth -= droneTotal;
+  if (droneTotal > 0) {
+    clickCount++;
+  }
+  statUpdate();
+  return droneTotal;
+}
+function startdroneInterval() {
+  var droneInterval = setInterval(collectDrone, 3000);
 
 }
 function collectReactor() {
@@ -93,23 +133,44 @@ function collectReactor() {
   regenTotal += (reactorUpgrades.reactor.multiplier * reactorUpgrades.reactor.quantity);
   autoRegenTotal = regenTotal;
   jaegerHealth += regenTotal;
+  statUpdate();
   return regenTotal;
 }
 function startregenInterval() {
-  var regenInterval = setInterval(collectReactor, 3000);
+  var regenInterval = setInterval(collectReactor, 2000);
 }
 function kaijuDeath() {
   if (kaijuHealth <= 0) {
+    level++;
+    kaijuHealth + 5;
     kaijuCores++;
-    if ((level % 5) == 0) {
-      drawCategoryFive()
-    }
-    else {
-      drawKaiju()
-    }
-    // draws new random kaiju with increased stats
+    drawKaiju()
   }
 }
+function randomKaiju() {
+  let index = Math.floor(Math.random() * 10);
+  kaiju = kaijuType[index]
+  drawKaiju()
+}
+function randomMap() {
+  let index = Math.floor(Math.random() * 5);
+  map = battleMap[index]
+  drawMap()
+}
+//Create kaiju index with id, picture, and battlemap
+function drawKaiju() {
+  document.getElementById('k-pic').innerHTML = kaiju.img
+  drawMap()
+  drawkaijuHealth()
+}
+function drawkaijuHealth() {
+  document.getElementById('k-health').innerText = kaijuHealth.toString()
+}
+function drawMap() {
+  document.getElementById('battlemap').innerHTML = map.img
+  drawMap()
+}
+
 function statUpdate() {
   let levelElem = document.getElementById("level");
   levelElem.innerText = level.toString();
@@ -118,7 +179,7 @@ function statUpdate() {
   kaijucoresElem.innerText = kaijuCores.toString();
   //increases kaiju cores number
   let jaegerhealthElem = document.getElementById("j-health");
-  jaegerhealthElem.innerText = jaegerHealth.toString()
+  jaegerhealthElem.innerText = jaeger_health.toString()
   //refreshes Jaeger Health
   let computercostElem = document.getElementById("comp-cost");
   computercostElem.innerText = shootUpgrades.computer.price.toString();
@@ -140,10 +201,21 @@ function statUpdate() {
   let healthtotalElem = document.getElementById("health-total");
   healthtotalElem.innerText = healthUpgrades.health.quantity.toString();
   //updates maintenence cost and quantity
-
+  let dronecostElem = document.getElementById("drone-cost");
+  dronecostElem.innerText = autoUpgrades.drones.price.toString();
+  let dronetotalElem = document.getElementById("drone-total");
+  dronetotalElem.innerText = autoUpgrades.drones.quantity.toString();
+  //updates drones cost and quantity
+  let clickcountElem = document.getElementById("click-count");
+  clickcountElem.innerText = clickCount.toString();
+  //updates Attack Counter
+  drawkaijuHealth()
 }
 
 
 startmissileInterval();
 startregenInterval();
+startdroneInterval();
+drawkaijuHealth()
+drawKaiju()
 statUpdate()
